6f1a5efcfb5a28fed61535baf120e4b049045a50
dreamanlan <dreaman_163@163.com>
2020-11-13 20:27:28 +0800

    update DslCalculator.cs

--- ElfPatch/Script/Commands.cs
+++ ElfPatch/Script/Commands.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using ElfPatch;
 
-namespace Calculator
+namespace DslExpression
 {
     //---------------------------------------------------------------------------------------------------------------
     internal class BeginCommand : SimpleExpressionBase
